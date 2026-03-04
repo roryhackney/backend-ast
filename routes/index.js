5e3ca24f07689b12a7f0c3aa57f0549e0aaffe4b
roryhackney <roryhackney@gmail.com>
2026-03-03 17:12:00 -0800

    convert from require to import

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-const express = require('express');
+import express from 'express';
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+export default router;
