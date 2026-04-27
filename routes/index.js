74c1ed91088276df74046e093e743ce17ea65041
roryhackney <roryhackney@gmail.com>
2026-04-26 20:22:02 -0700

    Adds simple login post route, tested with postman

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/test-api", function(req, res) {
+    const results = {test: "This is an API test", timestamp: new Date().toISOString()};
+    res.json(results);
+});
+
+router.post("/login", function(req, res) {
+    if (req.body === undefined) {
+        res.json({success: false});
+    } else {
+        const name = req.body.name;
+        const password = req.body.password;
+        if (name && password && name === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD) {
+            res.json({success: true});
+        } else {
+            res.json({success: false});
+        }
+    }
+});
+
 export default router;
