d74c06957ee6c542a93570c7dfd554e08226349d
roryhackney <roryhackney@gmail.com>
2026-05-03 20:36:04 -0700

    Route updates, unit tests in progress

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import router from '../routes/index';
 import request from 'supertest';
+import 'dotenv/config';
 
 const app = express();
-app.use('/', router);
+app.use(router);
 
 
 describe('Basic route test', () => {
@@ -13,3 +14,53 @@ describe('Basic route test', () => {
         expect(response.body.test).toBe("This is an API test");
     });
 });
+
+// describe('Login test', () => {
+//     //should not be 500 / error, should be 401 for unauthorized
+//     it('should not error with no body', async () => {
+//         const response = await request(app).post('/login');
+//         expect(response.status).toBe(401);
+//     });
+
+//     it('should not error with only username', async () => {
+//         const response = await request(app).post('/login').send({"username": process.env.ADMIN_USERNAME});
+//         expect(response.status).toBe(401);
+//     });
+
+//     it('should not error with only password', async () => {
+//         const response = await request(app).post('/login').send({"password": process.env.ADMIN_PASSWORD});
+//         expect(response.status).toBe(401);
+//     });
+
+//     it('should not log in with invalid admin username', async () => {
+//         const response = await request(app).post('/login').send({"username": "an-invalid-username", "password": process.env.ADMIN_PASSWORD});
+//         expect(response.status).toBe(401);
+//     });
+
+//     it('should not log in with invalid admin password', async () => {
+//         const response = await request(app).post('/login').send({"username": process.env.ADMIN_USERNAME, "password": "an-invalid-password"});
+//         expect(response.status).toBe(401);
+//     });
+// });
+
+describe('Login Test 2', () => {
+    it('should fucking work', async () => {
+        const response = await request(app).post('/login').send({username: process.env.ADMIN_USERNAME, password: process.env.ADMIN_PASSWORD});
+        expect(response.status).toBe(200);
+        expect(response.get("Set-Cookie")).toBeDefined();
+    });
+});
+
+// describe('Secure route test', () => {
+//     it('should block users not logged in', async () => {
+//         const response = await request(app).get('/secure-api');
+//         expect(await response.status).toBe(401);
+//     });
+
+//     //why doesn't this work
+//     it('should allow users that are logged in', async () => {
+//         //login
+//         expect(res.status).toBe(200);
+//         expect(res.body.superSecret).toBe('encryptedstring');
+//     });
+// });
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 import express from 'express';
 const router = express.Router();
 
-const authenticate = (username, password) => {
-    return {"success": username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD};
-}
-
 router.get('/', function(req, res, next) {
     res.status(200).render('index', { title: 'Express' });
 });
@@ -15,30 +11,31 @@ router.get("/test-api", function(req, res) {
 });
 
 router.get("/secure-api", function(req, res) {
-    if (!req.cookies.token) {
+    if (! req?.cookies?.token) {
         res.status(401).send();
     } else {
-        res.json({"superSecret": req.cookies.token});
+        res.status(200).json({"superSecret": req.cookies.token});
     }
-})
+});
 
 router.post("/login", function(req, res) {
-    if (req.body === undefined || req.body.name === undefined || req.body.password === undefined) {
-        res.json({success: false});
+    const user = req?.body?.username;
+    const pass = req?.body?.password;
+    if (user && pass &&
+        user === process.env.ADMIN_USERNAME && 
+        pass === process.env.ADMIN_PASSWORD) {
+        //TODO:
+        //generate a token
+        //set the cookie to remember the token
+        //put the token into the db, overwriting current session token
+        //invalidate other logins, deleting cookies?
+        //on navigating to page, if the cookie does not match db token, delete it and make them log in again
+        res.writeHead(200, {
+            "Set-Cookie": "token=encryptedstring; HttpOnly",
+            "Access-Control-Allow-Credentials": "true"
+        }).send();
     } else {
-        const result = authenticate(req.body.name, req.body.password);
-        if (result.success) {
-            //generate a token
-            //set the cookie to remember the token
-            //put the token into the db, overwriting current token
-            //invalidate other logins, deleting cookies?
-            res.writeHead(200, "oh boy login time", {
-                "Set-Cookie": "token=encryptedstring; HttpOnly",
-                "Access-Control-Allow-Credentials": "true"
-            }).send();
-        } else {
-            res.json(result);
-        }
+        res.status(401).send();
     }
 });
 
